e766c86a0b53ecd891c458f5880e42e25d8ad154
bifibrant <s.balchin@qub.ac.uk>
2025-02-19 13:26:45 +0000

    Initial Commit

---
+++ ninfty/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ninfty
+//
+//  Created by Scott Balchin on 19/02/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
